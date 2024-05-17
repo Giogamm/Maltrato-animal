d75ba738d94022367a61ed2ec3e5a72d0d068b9f
Giogamm <giovanymemije@gmail.com>
2024-05-16 18:47:26 -0600

    Actualizar ruta del archivo de estilos CSS en index.php y header.php

--- php/sections/header.php
+++ php/sections/header.php
@@ -1,19 +1,16 @@
 <header>
-
     <nav class="navbar">
         <div class="container">
             <ul class="nav-menu">
-
                 <h1>Maltrato animal</h1>
-                <li><a href="index.php">Inicio</a></li>
-                <li><a href="formas-de-maltrato-animal.php">Formas de maltrato animal</a></li>
-                <li><a href="como-ayudar.php">¿Como ayudar?</a></li>
+                <li class="<?= basename($_SERVER['PHP_SELF']) == 'index.php' ? 'activa' : '' ?>"><a href="index.php">Inicio</a></li>
+                <li class="<?= basename($_SERVER['PHP_SELF']) == 'formas-de-maltrato-animal.php' ? 'activa,' : '' ?>"><a href="formas-de-maltrato-animal.php">Formas de maltrato animal</a></li>
+                <li class="<?= basename($_SERVER['PHP_SELF']) == 'como-ayudar.php' ? 'activa' : '' ?>"><a href="como-ayudar.php">¿Como ayudar?</a></li>
                 <li><a href="poner aqui sobre los animales">Animales perdidos</a></li>
                 <li><a href="poner aqui algo">Iniciar sesión</a></li>
             </ul>
         </div>
     </nav>
     <div class="empuje">
-
     </div>
 </header>
