15fcb534743d62a780747aa98ed23566bcbe3f8d
Giogamm <giovanymemije@gmail.com>
2024-05-16 19:44:56 -0600

    Actualizar estilos CSS para mejorar la visualización de la lista y la superposición de imagen

--- php/sections/header.php
+++ php/sections/header.php
@@ -4,7 +4,7 @@
             <ul class="nav-menu">
                 <h1>Maltrato animal</h1>
                 <li class="<?= basename($_SERVER['PHP_SELF']) == 'index.php' ? 'activa' : '' ?>"><a href="index.php">Inicio</a></li>
-                <li class="<?= basename($_SERVER['PHP_SELF']) == 'formas-de-maltrato-animal.php' ? 'activa,' : '' ?>"><a href="formas-de-maltrato-animal.php">Formas de maltrato animal</a></li>
+                <li class="<?= basename($_SERVER['PHP_SELF']) == 'formas-de-maltrato-animal.php' ? 'activa' : '' ?>"><a href="formas-de-maltrato-animal.php">Formas de maltrato animal</a></li>
                 <li class="<?= basename($_SERVER['PHP_SELF']) == 'como-ayudar.php' ? 'activa' : '' ?>"><a href="como-ayudar.php">¿Como ayudar?</a></li>
                 <li><a href="poner aqui sobre los animales">Animales perdidos</a></li>
                 <li><a href="poner aqui algo">Iniciar sesión</a></li>
