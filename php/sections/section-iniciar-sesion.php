2c9fa9e1e35ff9620a831a07a37557020537e821
Giogamm <giovanymemije@gmail.com>
2024-05-19 00:42:44 -0600

    cambio de la referencia de las section de inicio y registro  php

--- php/sections/section-iniciar-sesion.php
+++ php/sections/section-iniciar-sesion.php
@@ -5,7 +5,7 @@
             <input type="text" name="usuario" placeholder="Usuario" required class="usuario">
             <input type="password" name="contrasena" placeholder="Contraseña" required class="contraseña">
             <input type="submit" value="Iniciar sesión" class="submit-sesion" id="iniciar-sesion">
-            <span>¿no tienes cuenta? <a href="registrarse.html">registrate</a></span>
+            <span>¿no tienes cuenta? <a href="registrarse.php">registrate</a></span>
         </form>
         <h3><a href="../index.php">Regresar</a></h3>
     </div>
--- php/sections/section-registrarse.php
+++ php/sections/section-registrarse.php
@@ -6,7 +6,7 @@
             <input type="email" name="correo" placeholder="Correo" class="correo">
             <input type="password" name="contrasena" placeholder="Contraseña" required class="contraseña">
             <input type="submit" value="Registrarse" class="submit">
-            <span>¿Ya tiene una cuenta? <a href="iniciar-sesion.html">Iniciar sesión</a></span>
+            <span>¿Ya tiene una cuenta? <a href="iniciar-sesion.php">Iniciar sesión</a></span>
             <h3><a href="../index.php">Regresar</a></h3>
         </form>
     </div>
