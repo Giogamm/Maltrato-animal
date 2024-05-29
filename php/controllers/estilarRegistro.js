d86582dae7c8ee223cc87d45f76431f64438fcac
magazabeth <7555582528az@gmail.com>
2024-05-29 13:27:50 -0600

    innecesario

--- php/controllers/estilarRegistro.js
+++
@@ -1,8 +0,0 @@
-// // Obtener el input de usuario
-// const usuarioInput = document.getElementById('usuario');
-
-// // Agregar un evento 'click' al input de usuario
-// usuarioInput.addEventListener('click', function() {
-//     // Cambiar el color del borde a azul
-//     usuarioInput.style.borderColor = 'blue';
-// });
