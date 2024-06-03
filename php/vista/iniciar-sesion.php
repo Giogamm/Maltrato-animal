3c7432a59e2f10001529fe1af1c420bca1ccd58d
magazabeth <7555582528az@gmail.com>
2024-06-03 12:22:56 -0600

    añadiendo cambios minimos al incio de sesión

--- php/vista/iniciar-sesion.php
+++ php/vista/iniciar-sesion.php
@@ -6,10 +6,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="author" content="Giovany Galeana Memije">
     <meta name="description" content="Pagina de inicio de sesión">
-    <link rel="stylesheet" href="css/iniciar-sesion.css">
+    <link rel="stylesheet" href="../../css/iniciar-sesion.css">
     <title>Iniciar sesión</title>
 </head>
 
-<?php  require_once 'php/sections/section-iniciar-sesion.php'; ?>
+<?php  require_once 'sections/section-iniciar-sesion.php'; ?>
 
 </html>
